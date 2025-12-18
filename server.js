cac775537fd4f4d0943e46bef9629ba5fe717a68
Goku216 <sagarpariyar@gmail.com>
2025-12-18 12:09:44 -0500

    domain changes

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 3000;
 // CORS (optional, but common)
 app.use(
   cors({
-     origin: ["http://localhost:3000", "http://127.0.0.1:3000", "exp://192.168.18.16:8082"], 
+     origin: ["http://localhost:3000", "http://127.0.0.1:3000", "exp://192.168.18.16:8082", "https://merohealth-web.vercel.app"], 
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
